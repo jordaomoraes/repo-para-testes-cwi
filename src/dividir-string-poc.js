21272a70d13120ef7f0d9eaafd9fba2da6d51bf2
Guilherme Jordão <62813340+jordaomoraes@users.noreply.github.com>
2022-08-09 17:08:26 -0300

    chore:testes ternario

--- src/dividir-string-poc.js
+++ src/dividir-string-poc.js
@@ -3,25 +3,33 @@ const RetiraAssentos = require('./retira-assentos')
 
 const data = {
 
-	TaxationGroupForDestination__r: {
-		attributes: {
-			type: "SX5__c",
-			url: "/services/data/v54.0/sobjects/SX5__c/a0I5e000008EAtyEAG"
-		},
-		ExternalID__c: "21|4GR"
-	}
+	TaxationGroupForDestination__r: null
+	
+
+  // return status === 0
+  //   ? 'default'
+  //   : status === 1
+  //     ? 'icon-1'
+  //     : 'icon-2';
+
+	// TaxationGroupForDestination__r: {
+	// 	attributes: {
+	// 		type: "SX5__c",
+	// 		url: "/services/data/v54.0/sobjects/SX5__c/a0I5e000008EAtyEAG"
+	// 	},
+	// 	ExternalID__c: "21|4GR"
+	// }
 }
+const string = data.TaxationGroupForDestination__r === null ? null : 
+(data.TaxationGroupForDestination__r.ExternalID__c).split('|');
 
-const string = data.TaxationGroupForDestination__r === null ? null : (data.TaxationGroupForDestination__r.ExternalID__c).split('|') ;
-
-// const resultado = string.split('|')
-
-
+// const taxationGroup = dadosCadastrais.TaxationGroup__r === null ? null : (dadosCadastrais.TaxationGroup__r.ExternalID__c).split('|'); 
+const teste =  string === null ? null : (string[1])
 class dividirStringPoc {
 
 	async Testes() {
 
-		console.log(`aaa dividir -> ${string[1]} `)
+		console.log(` dividir -> ${teste} `)
 	}
 
 
